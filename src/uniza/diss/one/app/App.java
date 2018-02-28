f469ce1d7c34c03b90994f64063a50b1a2cdb07d
mariokemen <mario.kemen@gmail.com>
2018-02-28 20:09:48 +0100

    commit message

--- src/uniza/diss/one/app/App.java
+++ src/uniza/diss/one/app/App.java
@@ -92,7 +92,6 @@ public class App extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new App().setVisible(true);
-
             }
         });
     }
